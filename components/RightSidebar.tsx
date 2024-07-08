a6d749f06cbd0d43f959c08a2404d6922fc31329
Armond-Guze <Armond.Guze@yahoo.com>
2024-07-07 23:43:07 -0400

    Implement sentry, minor homepage fixes

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -10,11 +10,13 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
         <div className='profile-banner'/>
         <div className='profile'>
             <div className='profile-img'>
-            <span className='text-5xl font-bold text-blue-500'>{user.name[0]}</span>
+            <span className='text-5xl font-bold text-blue-500'>
+            {user.name[0]}
+            </span>
             </div>
             <div className='profile-details'>
             <h1 className='profile-name'>
-                {user.name}
+            {user.email}
             </h1>
             <p className='profile-email'>
             {user.email}
